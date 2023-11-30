d271ed065ec206afcd7ad13d5dbdef146ea296a9
niklasfomin <niklas.fomin@campus.tu-berlin.de>
2023-11-30 11:08:50 +0100

    fix stuff

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -58,12 +58,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1699610813524
+    static public final long APP_TIMESTAMP = 1700771487280
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5809
+    static public final int APP_BUILDNUM = 5810
 
     /**
      * The app build time string relative to UTC timezone
